0596462385534056a0e5cd20030c10130f679091
Jakub <landowski.jakub18@gmail.com>
2023-09-18 15:36:28 +0200

    maj dossier

--- exo30/exo30.js
+++
@@ -1,9 +0,0 @@
-let tab=[],
-    nb
-
-
-
-while (nb!=777) {
-   tab = prompt("Veuillez indiquer un chiffre")
-
-}
--- tpPOO/script.js
+++
@@ -1,8 +0,0 @@
-import { Voiture } from "./Voiture.js";
-
-import { Moto } from "./Moto.js"
-
-let moto = new Moto("BMW", "R1150R Rockster", 65000 , 2005)
-let voiture = new Voiture("Renalut", "Kangoo", 240000, 2003 , "Climatisée")
-
-moto
