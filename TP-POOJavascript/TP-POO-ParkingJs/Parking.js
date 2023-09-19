f3fa34aaa1a5c17a983531dfaab88dccbd67dd3e
Jakub <landowski.jakub18@gmail.com>
2023-09-19 11:29:51 +0200

    maj exo

--- TP-POOJavascript/TP-POO-ParkingJs/Parking.js
+++ TP-POOJavascript/TP-POO-ParkingJs/Parking.js
@@ -14,7 +14,25 @@ export class Parking {
     pay(plate){
         this.tabParking.forEach(element => {
             if (plate === element.plate) {
-                
+                const today = new Date()
+
+
+                let duree = element.date - today
+                duree = (duree/1000) / 60
+                if (duree <= 15) {
+                    console.log("0.8€")
+                }
+                if (duree > 15 && duree <= 30) {
+                    console.log("1.30€")
+                }
+                if (duree > 30 && duree<=45) {
+                    console.log("1.7€")
+                }
+                if (duree> 45) {
+                    console.log("6€")
+                }
+
+            
             }
            
         });
