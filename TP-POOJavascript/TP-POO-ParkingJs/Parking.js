cf80fd66e6786b96d0b11455c8ed5a8470f6950e
Jakub <landowski.jakub18@gmail.com>
2023-09-19 14:07:46 +0200

    maj exo

--- TP-POOJavascript/TP-POO-ParkingJs/Parking.js
+++ TP-POOJavascript/TP-POO-ParkingJs/Parking.js
@@ -1,45 +1,49 @@
-import {Voiture} from "./Voiture.js"
+import { Voiture } from "./Voiture.js"
 
 export class Parking {
-    constructor(){
+    constructor() {
         this.tabParking = []
     }
 
     inTab(plate) {
-        let vehicule = new Voiture(plate)
-        this.tabParking.push(vehicule)
-        console.log(this.tabParking)
+        if (plate === "") {
+            alert("la plaque n'est pas valide")
+        } else {
+            let vehicule = new Voiture(plate)
+            this.tabParking.push(vehicule)
+            console.log(this.tabParking)
+        }
     }
 
-    pay(plate){
+    pay(plate) {
         let bool = false;
 
         this.tabParking.forEach(element => {
-            if (plate === element.plate ) {
+            if (plate === element.plate) {
                 bool = true;
                 const today = new Date()
 
 
                 let duree = element.date - today
-                duree = (duree/1000) / 60
+                duree = (duree / 1000) / 60
                 if (duree <= 15) {
                     console.log("0.8€")
                 }
                 if (duree > 15 && duree <= 30) {
                     console.log("1.30€")
                 }
-                if (duree > 30 && duree<=45) {
+                if (duree > 30 && duree <= 45) {
                     console.log("1.7€")
                 }
-                if (duree> 45) {
+                if (duree > 45) {
                     console.log("6€")
                 }
 
-            
+
             }
-           
+
         });
-        if (bool === false ) {
+        if (bool === false) {
             alert("la voiture n'existe pas")
         }
     }
