19f5605f39ca834b13051fd433615de39375ade0
Jakub <landowski.jakub18@gmail.com>
2023-11-16 10:53:19 +0100

    exo java

--- variable_java/src/main/java/org/example/Main.java
+++ variable_java/src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ import org.example.exo2_3.Exo2_3;
 import org.example.exo2_4.Exo2_4;
 import org.example.exo3_1.Exo3_1;
 import org.example.exo3_2.Exo3_2;
+import org.example.exo3_3.Exo3_3;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -15,6 +16,7 @@ public class Main {
        // Exo2_3.getReadWrite();
        // Exo2_4.getReadWrite();
        // Exo3_1.getReadWrite();
-        Exo3_2.getReadWrite();
+       // Exo3_2.getReadWrite();
+        Exo3_3.getReadWrite();
     }
 }
---
+++ variable_java/src/main/java/org/example/exo3_3/Exo3_3.java
@@ -0,0 +1,32 @@
+package org.example.exo3_3;
+
+import java.util.Scanner;
+
+public class Exo3_3 {
+
+    public static void getReadWrite() {
+
+        Scanner scannerNom = new Scanner(System.in);
+        System.out.println("Quel est votre prénom ?");
+        String nom = scannerNom.next();
+
+
+        Scanner scannerNomDeux = new Scanner(System.in);
+        System.out.println("Quel est votre prénom ?");
+        String nomDeux = scannerNomDeux.next();
+
+
+        Scanner scannerNomTrois = new Scanner(System.in);
+        System.out.println("Quel est votre prénom ?");
+        String nomTrois = scannerNomTrois.next();
+
+
+        if (nom.compareTo(nomDeux) <= 0 || (nomDeux.compareTo(nomTrois)) <= 0 ) {
+            System.out.println("C'est  dans l'ordre ") ;
+
+        } else {System.out.println("C'est pas dans l'ordre ") ;}
+
+
+
+    }
+}
