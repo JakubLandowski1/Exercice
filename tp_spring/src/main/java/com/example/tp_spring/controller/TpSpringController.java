f876830ae15ce8aa2e68914f71bffb8f241f1680
Jakub <landowski.jakub18@gmail.com>
2024-02-14 11:02:42 +0100

    tp srping

--- tp_spring/src/main/java/com/example/tp_spring/controller/TpSpringController.java
+++ tp_spring/src/main/java/com/example/tp_spring/controller/TpSpringController.java
@@ -1,19 +1,61 @@
 package com.example.tp_spring.controller;
 
 
+import com.example.tp_spring.model.Student;
+import com.example.tp_spring.service.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
 
 public class TpSpringController {
 
-    @GetMapping(value = "/accueil")
+    private final StudentService studentService;
+
+    @GetMapping
     public String accueil() {
         return "accueil";
     }
 
+    @GetMapping("detail/{studentId}")
+    public String detailStudent(@PathVariable("studentId")UUID id, Model model) {
+        Student student = studentService.getStudentById(id);
+        model.addAttribute("nomStudent", student);
+        return "detailStudent";
+    }
 
+    @GetMapping("/list")
+    public String listingStudents(Model model) {
+        List<Student> students = studentService.getStudents();
+        model.addAttribute("students", students);
+        return "detailsEtudiant";
+    }
+
+    @GetMapping("/add")
+    public String addStudent(Model model) {
+        model.addAttribute("student", new Student());
+        return "templates/inscription";
+    }
+
+    @GetMapping("/show")
+    public String showStudent(@RequestParam(name = "namestudent", required = false) String name, Model model) {
+        System.out.println(name);
+        Student student = studentService.getStudentByName(name);
+
+        if (student != null) {
+            model.addAttribute("etudiant", student);
+            return "detailsEtudiant";
+        } else {
+            return "detailsEtudiantsSiVide";
+        }
+
+    }
 }
--- tp_spring/src/main/java/com/example/tp_spring/service/StudentService.java
+++ tp_spring/src/main/java/com/example/tp_spring/service/StudentService.java
@@ -1,4 +1,62 @@
 package com.example.tp_spring.service;
 
+import com.example.tp_spring.model.Student;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@Service
 public class StudentService {
+    private final Map<UUID, Student> students;
+
+
+    public StudentService() {
+        students = new HashMap<>();
+
+        Student studentOne = Student.builder()
+                .id(UUID.randomUUID())
+                .firstname("Jean")
+                .lastname("Dupont")
+                .age(23)
+                .email("jeanDupont@mail.com")
+                .build();
+
+
+        Student studentTwo = Student.builder()
+                .id(UUID.randomUUID())
+                .firstname("Pierre")
+                .lastname("Dufour")
+                .age(23)
+                .email("PierreDufour@mail.com")
+                .build();
+
+
+        Student studentThree = Student.builder()
+                .id(UUID.randomUUID())
+                .firstname("Alice")
+                .lastname("Dupont")
+                .age(23)
+                .email("AliceDpnt@mail.com")
+                .build();
+
+        students.put(studentOne.getId(), studentOne);
+        students.put(studentTwo.getId(), studentTwo);
+        students.put(studentThree.getId(), studentThree);
+    }
+
+    public List<Student> getStudents(){
+        return students.values().stream().toList();
+    }
+
+    public Student getStudentByName (String name) {
+        return students.values().stream().filter(r-> r.getLastname().equals(name)).findFirst().orElse(null);
+    }
+
+    public Student getStudentById(UUID id) {
+        return students.values().stream().filter(s -> s.getId().equals(id)).findFirst().orElse(null);
+    }
+
 }
