590bcab56e4f9eb22a1113ff5d822a4e296b29ab
Jakub <landowski.jakub18@gmail.com>
2024-02-20 10:29:09 +0100

    ajout tp blog

--- demos/tp_blog/src/main/java/com/example/tp_blog/mapper/PostMapper.java
+++ demos/tp_blog/src/main/java/com/example/tp_blog/mapper/PostMapper.java
@@ -1,7 +1,22 @@
 package com.example.tp_blog.mapper;
 
+import com.example.tp_blog.entities.Post;
+import com.example.tp_blog.model.PostDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface PostMapper {
+
+    @Mapping(source= "nom", target = "nom")
+    @Mapping(source = "email", target = "email")
+    @Mapping(source = "contentPost", target = "contentPost")
+    PostDTO postToPostDTO(Post post);
+
+    @Mapping(source= "nom", target = "nom")
+    @Mapping(source = "email", target = "email")
+    @Mapping(source = "contentPost", target = "contentPost")
+    Post PostDtoToPost(PostDTO dto);
+
+
 }
